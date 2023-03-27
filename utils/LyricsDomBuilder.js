710e857a7fa3002f5b7529af16d84f2539242aef
Fabrizio <65259076+Fabrizz@users.noreply.github.com>
2023-03-27 11:08:27 -0300

    Revert "Revert "Merge branch 'dev' of https://github.com/Fabrizz/MMM-LiveLyrics into dev""
    This reverts commit 580c2ff0f6fc27ba7caf1bec58137dd7e5ec8bf0.

--- utils/LyricsDomBuilder.js
+++ utils/LyricsDomBuilder.js
@@ -4,12 +4,15 @@ class LyricsDomBuilder {
     this.config = config;
     this.translate = translator; // Remove later as transalation is done on entry point
     this.root = document.querySelector(":root");
+    this.backgroundColors = ["V", "DV", "LV", "M", "DM", "LM"];
 
     this.icons = {
       icon: "M21,3V15.5A3.5,3.5 0 0,1 17.5,19A3.5,3.5 0 0,1 14,15.5A3.5,3.5 0 0,1 17.5,12C18.04,12 18.55,12.12 19,12.34V6.47L9,8.6V17.5A3.5,3.5 0 0,1 5.5,21A3.5,3.5 0 0,1 2,17.5A3.5,3.5 0 0,1 5.5,14C6.04,14 6.55,14.12 7,14.34V6L21,3Z",
       load: "M2 12C2 16.97 6.03 21 11 21C13.39 21 15.68 20.06 17.4 18.4L15.9 16.9C14.63 18.25 12.86 19 11 19C4.76 19 1.64 11.46 6.05 7.05C10.46 2.64 18 5.77 18 12H15L19 16H19.1L23 12H20C20 7.03 15.97 3 11 3C6.03 3 2 7.03 2 12Z",
       warning:
         "M13,2V4C17.39,4.54 20.5,8.53 19.96,12.92C19.5,16.56 16.64,19.43 13,19.88V21.88C18.5,21.28 22.45,16.34 21.85,10.85C21.33,6.19 17.66,2.5 13,2M11,2C9.04,2.18 7.19,2.95 5.67,4.2L7.1,5.74C8.22,4.84 9.57,4.26 11,4.06V2.06M4.26,5.67C3,7.19 2.24,9.04 2.05,11H4.05C4.24,9.58 4.8,8.23 5.69,7.1L4.26,5.67M2.06,13C2.26,14.96 3.03,16.81 4.27,18.33L5.69,16.9C4.81,15.77 4.24,14.42 4.06,13H2.06M7.06,18.37L5.67,19.74C7.18,21 9.04,21.79 11,22V20C9.58,19.82 8.23,19.25 7.1,18.37H7.06M13,13V7H11V13H13M13,17V15H11V17H13Z",
+      search:
+        "M15.5,12C18,12 20,14 20,16.5C20,17.38 19.75,18.21 19.31,18.9L22.39,22L21,23.39L17.88,20.32C17.19,20.75 16.37,21 15.5,21C13,21 11,19 11,16.5C11,14 13,12 15.5,12M15.5,14A2.5,2.5 0 0,0 13,16.5A2.5,2.5 0 0,0 15.5,19A2.5,2.5 0 0,0 18,16.5A2.5,2.5 0 0,0 15.5,14M5,3H19C20.11,3 21,3.89 21,5V13.03C20.5,12.23 19.81,11.54 19,11V5H5V19H9.5C9.81,19.75 10.26,20.42 10.81,21H5C3.89,21 3,20.11 3,19V5C3,3.89 3.89,3 5,3M7,7H17V9H7V7M7,11H12.03C11.23,11.5 10.54,12.19 10,13H7V11M7,15H9.17C9.06,15.5 9,16 9,16.5V17H7V15Z",
     };
   }
 
@@ -93,11 +96,98 @@ class LyricsDomBuilder {
     return wrapper;
   }
 
-  paint(str) {
+  paint(current, lyrics) {
     const wrapper = this.globalWrapper();
-    const content = document.createElement("div");
-    content.innerText = str;
-    wrapper.appendChild(content);
+    const main = document.createElement("div");
+    main.classList.add(
+      "main",
+      "theme-type",
+      "scroll-type",
+      "style-type",
+      current
+        ? lyrics
+          ? lyrics.lyrics
+            ? "found"
+            : "missing"
+          : "searching"
+        : "unknown",
+    );
+    const backdrop = document.createElement("div");
+    backdrop.classList.add("backdrop");
+    const sub = document.createElement("div");
+    sub.classList.add("sub");
+    const view = document.createElement("div");
+    view.classList.add("view");
+    const blobs = document.createElement("div");
+    blobs.classList.add("blobs");
+    const grid = this.getGridOfBlobs();
+
+    /*const header = document.createElement("div");
+    header.classList.add("header");
+    header.id = "LILY-SOCKET-CODE";*/
+
+    const media = document.createElement("div");
+    media.classList.add("media");
+    const cover = document.createElement("div");
+    cover.classList.add("cover");
+    cover.style.backgroundImage = `url(${current.image})`;
+    const info = document.createElement("div");
+    info.classList.add("info");
+    const title = document.createElement("div");
+    title.classList.add("title");
+    title.innerText = current.name;
+    const subtitle = document.createElement("div");
+    subtitle.classList.add("subtitle");
+    subtitle.innerText = current.artists;
+
+    media.appendChild(cover);
+    info.appendChild(title);
+    info.appendChild(subtitle);
+    media.appendChild(info);
+
+    const container = document.createElement("div");
+    container.classList.add("container");
+    const ly = document.createElement("div");
+    ly.classList.add("ly");
+
+    if (lyrics) {
+      if (lyrics.lyrics) {
+        ly.innerText = lyrics.lyrics;
+      } else {
+        media.classList.add("extended");
+
+        const nolyrics = document.createElement("div");
+        nolyrics.classList.add("nolyrics");
+        nolyrics.innerText = "NOT FOUND";
+
+        /*view.appendChild(header);*/
+        view.appendChild(nolyrics);
+        view.appendChild(media);
+        blobs.appendChild(grid);
+        sub.appendChild(blobs);
+        sub.appendChild(view);
+        main.appendChild(sub);
+        main.appendChild(backdrop);
+        wrapper.appendChild(main);
+        return wrapper;
+      }
+    } else {
+      const search = document.createElement("div");
+      search.classList.add("search");
+      search.innerText = "SEARCHING SONG";
+      ly.appendChild(search);
+    }
+
+    /*view.appendChild(header);*/
+    container.appendChild(ly);
+    view.appendChild(container);
+    view.appendChild(media);
+    blobs.appendChild(grid);
+    sub.appendChild(blobs);
+    sub.appendChild(view);
+    main.appendChild(sub);
+    main.appendChild(backdrop);
+    wrapper.appendChild(main);
     return wrapper;
   }
 
@@ -176,5 +266,17 @@ class LyricsDomBuilder {
       height: 200,
     }).svg();
     return container;
+  }
+
+  getGridOfBlobs() {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+    this.backgroundColors.forEach((bg) => {
+      const sq = document.createElement("div");
+      sq.classList.add("blob", `blob-${bg}`);
+      sq.style.gridArea = bg;
+      grid.appendChild(sq);
+    });
+    return grid;
   }
 }
